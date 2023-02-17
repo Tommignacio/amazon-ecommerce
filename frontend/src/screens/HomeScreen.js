c3565a73d33f6952fadc338ea4d7c86473f9f238
Tommignacio <88063640+Tommignacio@users.noreply.github.com>
2023-02-16 23:42:42 -0300

    adding react-bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,42 +1,40 @@
 import { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import logger from 'use-reducer-logger';
-
+// import logger from "use-reducer-logger";
 
 const reducer = (state, action) => {
 	switch (action.type) {
 		case "FETCH_REQUEST":
-			return { ...state, loading: true }
-		case 'FETCH_SUCCESS':
-			return { ...state, loading: false, products: action.payload }
-		case 'FETCH_FAIL':
-			return { ...state, loading: false, error: action.payload }
+			return { ...state, loading: true };
+		case "FETCH_SUCCESS":
+			return { ...state, loading: false, products: action.payload };
+		case "FETCH_FAIL":
+			return { ...state, loading: false, error: action.payload };
 		default:
 			return state;
 	}
-}
-
+};
 
 function HomeScreen() {
 	//save products in stateReducer from backend
-	const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+	const [{ loading, error, products }, dispatch] = useReducer(reducer, {
 		products: [],
 		loading: true,
 		error: "",
-	})
+	});
 	//load only after the component is rendered
 	useEffect(() => {
 		const fetchData = async () => {
-			dispatch({ type: 'FETCH_REQUEST' });
+			dispatch({ type: "FETCH_REQUEST" });
 			try {
 				//get products from backend
 				const result = await axios.get("/api/products");
-				dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+				dispatch({ type: "FETCH_SUCCESS", payload: result.data });
 			} catch (error) {
-				dispatch({ type: "FETCH_FAIL", payload: error.message })
+				dispatch({ type: "FETCH_FAIL", payload: error.message });
 			}
-		}
+		};
 		fetchData();
 	}, []);
 
@@ -44,28 +42,29 @@ function HomeScreen() {
 		<div>
 			<h1>Featured Products</h1>
 			<div className="products">
-				{loading ?
+				{loading ? (
 					<div>loading...</div>
-					: error ?
-						<div>{error}</div>
-						:
-						products.map((product) => (
-							<div className="product" key={product.slug}>
-								{/* Link replaces <a> to work on a single page app   */}
+				) : error ? (
+					<div>{error}</div>
+				) : (
+					products.map((product) => (
+						<div className="product" key={product.slug}>
+							{/* Link replaces <a> to work on a single page app   */}
+							<Link to={`/product/${product.slug}`}>
+								<img src={product.image} alt={product.name} />
+							</Link>
+							<div className="product-info">
 								<Link to={`/product/${product.slug}`}>
-									<img src={product.image} alt={product.name} />
+									<p>{product.name}</p>
 								</Link>
-								<div className="product-info">
-									<Link to={`/product/${product.slug}`}>
-										<p>{product.name}</p>
-									</Link>
-									<p>
-										<strong>${product.price} </strong>
-									</p>
-									<button> Add to cart</button>
-								</div>
+								<p>
+									<strong>${product.price} </strong>
+								</p>
+								<button> Add to cart</button>
 							</div>
-						))}
+						</div>
+					))
+				)}
 			</div>
 		</div>
 	);
