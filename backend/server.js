bc94fa005c3bc386ae4e4a929e9d90c64b0f6653
Tommignacio <tomydominguez96@gmail.com>
2023-02-16 21:31:00 -0300

    replace useState with useeduce

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import data from "./data.js";
-const app = express();
 
-app.get("api/products", (req, res) => {
+const app = express();
+//test
+app.get('/api/products', (req, res) => {
 	res.send(data.products);
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 app.listen(port, () => {
 	console.log(`serve at http://localhost:${port}`);
 });
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-//import data from "../data";
+import logger from 'use-reducer-logger';
+
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "FETCH_REQUEST":
+			return { ...state, loading: true }
+		case 'FETCH_SUCCESS':
+			return { ...state, loading: false, products: action.payload }
+		case 'FETCH_FAIL':
+			return { ...state, loading: false, error: action.payload }
+		default:
+			return state;
+	}
+}
+
 
 function HomeScreen() {
-	//save products in state from backend
-	const [products, setProducts] = useState([]);
+	//save products in stateReducer from backend
+	const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+		products: [],
+		loading: true,
+		error: "",
+	})
 	//load only after the component is rendered
 	useEffect(() => {
-		//get products from backend
 		const fetchData = async () => {
-			const result = await axios.get("/api/products");
-			setProducts(result.data);
-		};
+			dispatch({ type: 'FETCH_REQUEST' });
+			try {
+				//get products from backend
+				const result = await axios.get("/api/products");
+				dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+			} catch (error) {
+				dispatch({ type: "FETCH_FAIL", payload: error.message })
+			}
+		}
 		fetchData();
 	}, []);
 
@@ -20,23 +44,28 @@ function HomeScreen() {
 		<div>
 			<h1>Featured Products</h1>
 			<div className="products">
-				{products.map((product) => (
-					<div className="product" key={product.slug}>
-						{/* Link replaces <a> to work on a single page app   */}
-						<Link to={`/product/${product.slug}`}>
-							<img src={product.image} alt={product.name} />
-						</Link>
-						<div className="product-info">
-							<Link to={`/product/${product.slug}`}>
-								<p>{product.name}</p>
-							</Link>
-							<p>
-								<strong>${product.price} </strong>
-							</p>
-							<button> Add to cart</button>
-						</div>
-					</div>
-				))}
+				{loading ?
+					<div>loading...</div>
+					: error ?
+						<div>{error}</div>
+						:
+						products.map((product) => (
+							<div className="product" key={product.slug}>
+								{/* Link replaces <a> to work on a single page app   */}
+								<Link to={`/product/${product.slug}`}>
+									<img src={product.image} alt={product.name} />
+								</Link>
+								<div className="product-info">
+									<Link to={`/product/${product.slug}`}>
+										<p>{product.name}</p>
+									</Link>
+									<p>
+										<strong>${product.price} </strong>
+									</p>
+									<button> Add to cart</button>
+								</div>
+							</div>
+						))}
 			</div>
 		</div>
 	);
